4c131767d62a635e1e2fad5ccaba1f2b079f1fbd
Paul <p.burns2@exeter.ac.uk>
2020-03-27 10:28:32 +0000

    update

--- DispersionRelation.py
+++ DispersionRelation.py
@@ -14,21 +14,31 @@ Nz = 180
 k = np.arange(Nx)*2*np.pi/Lx
 n = np.arange(Nz)*np.pi/Lz
 
+print(type(k),k.shape)
+
 #Compute Dispersion Relation:
 DR = np.zeros( (Nx,Nz,len(N_vec)) )
 for nn in range(0,len(N_vec)):
     for ii in range(0,Nx):
         for jj in range(0,Nz):
-            kvec = (k[ii],n[jj])
-            DR[ii,jj,nn] = np.abs(k[ii])/np.abs(kvec)*N_vec[nn]
+            kvec = np.array([k[ii],n[jj]])
+            DR[ii,jj,nn] = np.abs(k[ii])/np.linalg.norm(kvec)*N_vec[nn]
 
 
-plt.plot(n,DR[40,:,0])
-plt.plot(n,DR[40,:,2])
-plt.plot(n,DR[40,:,4])
-plt.plot(n,DR[40,:,6])
-plt.plot(n,DR[40,:,8])
-plt.plot(n,DR[40,:,10])
-plt.savefig(DR.png)
+kIdx=2
+#kIdx=20
+
+plt.title(r'$k^{\prime}/Lx=$'+str(int(round(k[kIdx]/(2*np.pi))))+' (1/m)')
+plt.xlabel(r'$n$ (rad/m)')
+plt.ylabel(r'$\Lambda_{\bf k}^{\alpha}$ (rad/s)')
+plt.plot(n,DR[kIdx,:,0], label=str(N_vec[0]))
+plt.plot(n,DR[kIdx,:,2])
+plt.plot(n,DR[kIdx,:,4])
+plt.plot(n,DR[kIdx,:,6])
+plt.plot(n,DR[kIdx,:,8])
+plt.plot(n,DR[kIdx,:,10])
+plt.plot(n,DR[kIdx,:,12], label=str(N_vec[12]))
+plt.legend(title=r'$N$ (rad/s)')
+plt.show()
 
 
