6db50038060832e64ee187e7469bf78c0230df2c
Paul <p.burns2@exeter.ac.uk>
2020-10-27 16:51:33 +0000

    add submit script for ISCA with SLURM

---
+++ dedalus/submit_isca_dedalus.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --export=ALL # export all environment variables to the batch job
+#SBATCH –D . # set working directory to .
+#SBATCH –p ptq
+#SBATCH --time=00:10:00 # maximum walltime for the job
+#SBATCH –A Research_Project-183035 # research project to submit under
+#SBATCH --nodes=1 # specify number of nodes
+#SBATCH --ntasks-per-node=16 # specify number of processors per node
+#SBATCH --mail-type=END # send email at job completion
+#SBATCH --mail-user=p.burns2@exeter.ac.uk
+
+echo '<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>'
+echo PBS_O_HOST      = $SLURM_SUBMIT_HOST
+echo PBS_NODEFILE    = $SLURM_JOB_NODELIST
+echo '<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>'
+
+DedalusPath="/home/ubuntu/dedalus"
+ExecPath="/home/ubuntu/BoussinesqLab/dedalus/plotMultipleN.bsh"
+
+module purge
+module load Python/3.5.2-foss-2016b
+. "$DedalusPath/bin/activate"
+export OMPI_MCA_mpi_warn_on_fork=0
+
+echo -e 'Running Dedalus...'
+mpirun -np 16 python3 $ExecPath
